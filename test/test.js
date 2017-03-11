c57ec111f5c1cc565673b1f58bd2cc62351f6bbb
Joshua Escamilla <joshua.escamilla@selu.edu>
2017-03-10 18:57:31 -0600

    Create Mocha unit tests for evaluation of arithmetic expressions

---
+++ test/test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+
+const Parser = require('../Parser');
+const Evaluator = require('../Evaluator');
+
+function interpret(input) {
+  const parser = new Parser(input);
+  const ast = parser.parseExpression();
+  const evaluator = new Evaluator(ast);
+  const output = evaluator.evaluate();
+  return output.value;
+}
+
+describe('interpret()', function () {
+  const tests = [
+    {input: '1', expected: 1},
+    {input: '-1', expected: -1},
+    {input: '(+ 1 2)', expected: 3},
+    {input: '(- 3 2)', expected: 1},
+    {input: '(* 2 3)', expected: 6},
+    {input: '(/ 6 3)', expected: 2},
+    {input: '(+ (+ 1 2) 3)', expected: 6},
+  ];
+
+  tests.forEach(function (test) {
+    it(`correctly evaluates ${test.input}`, function () {
+      const actual = interpret(test.input);
+      assert.equal(actual, test.expected);
+    });
+  });
+});
