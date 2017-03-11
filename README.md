3fc00bfb0dd4cb30c3c19297e6e7745ff53ef605
Joshua Escamilla <joshua.escamilla@selu.edu>
2017-03-10 19:59:26 -0600

    Add `eval` command to allow user to evaluate any expression with the CLI

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 Evaluate arithmetic expressions written in parenthesized prefix notation.
 
+## Usage
+
 ```
-# evaluate an example expression and display the parse tree
-npm run demo
+# evaluate an expression and display the result
+npm run eval '(+ 1 2)'
+
+# use the `-v | --verbose` flag to show more details including the parse tree
+npm run eval -- -v '(+ 1 2)'
 
 # run unit tests
 npm test
--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const util = require('util');
 
+const parseArgs = require('minimist');
+const argv = parseArgs(process.argv.slice(2), {
+  boolean: 'verbose',
+  alias: {
+    v: 'verbose'
+  }
+});
+
 const Parser = require('./parser');
 const Evaluator = require('./evaluator');
 
-const input = "(* (+ 1 2) 3)";
+if (argv._.length === 0) {
+  console.log('no expression given');
+  process.exit();
+}
+
+const input = argv._.shift();
 
 const parser = new Parser(input);
 const ast = parser.parseExpression();
@@ -11,5 +24,9 @@ const ast = parser.parseExpression();
 const evaluator = new Evaluator(ast);
 const output = evaluator.evaluate().value;
 
-const details = { input, output, ast };
-console.log(util.inspect(details, { depth: null, colors: true}));
+if (argv.verbose) {
+  const details = { input, output, ast };
+  console.log(util.inspect(details, { depth: null, colors: true}));
+} else {
+  console.log(output);
+}
